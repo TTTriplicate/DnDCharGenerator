6ee6beeee37d3ae63370918e14091e1fd08e67ce
TTTriplicate <45953087+TTTriplicate@users.noreply.github.com>
2019-04-05 20:13:46 -0400

    Update Profession.cs
    added interactive constructor as static method

--- DnDClassesTest/DnDClassesTest/Professions/Profession.cs
+++ DnDClassesTest/DnDClassesTest/Professions/Profession.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace DnDClassesTest
 {
@@ -90,6 +91,64 @@ namespace DnDClassesTest
         public abstract bool[] SavingThrows();
 
         public abstract List<string> ClassFeatures();
+
+        public static Profession InteractiveChoice()
+        {
+            int p = 1, proPath = 9, level = 12;
+            Profession CharClass = new Bard();
+
+            using (var first = new StatsAndProfession())
+            {
+                var result = first.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    p = first.p;
+                    proPath = first.proPath;
+                    level = first.level;
+                }
+                switch (p)
+                {
+                    case 0:
+                        CharClass = new Barbarian(level, proPath);
+                        foreach (string i in CharClass.CurrentFeatures())
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
+                    case 1:
+                        CharClass = new Bard(level, proPath);
+                        break;
+                    case 2:
+                        CharClass = new Cleric(level, proPath);
+                        break;
+                    case 3:
+                        CharClass = new Druid(level, proPath);
+                        break;
+                    case 4:
+                        CharClass = new Fighter(level, proPath);
+                        break;
+                    case 5:
+                        CharClass = new Monk(level, proPath);
+                        break;
+                    case 6:
+                        CharClass = new Paladin(level, proPath);
+                        break;
+                    case 7:
+                        CharClass = new Ranger(level, proPath);
+                        break;
+                    case 8:
+                        CharClass = new Rogue(level, proPath);
+                        break;
+                    case 9:
+                        CharClass = new Sorcerer(level, proPath);
+                        break;
+                    case 10:
+                        CharClass = new Wizard(level, proPath);
+                        break;
+                }
+            }
+            return CharClass;
+        }
 
         public bool LevelUp()
         {
