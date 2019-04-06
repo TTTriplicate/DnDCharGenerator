631fc212892d23c95dc4547d513f1bcfad650faa
TTTriplicate <45953087+TTTriplicate@users.noreply.github.com>
2019-04-05 20:12:14 -0400

    two new forms
    Form for selection of profession and abilities(forgot to output ability scores tho), and for skill selection.  Not sure how best to implement yet.

---
+++ DnDClassesTest/DnDClassesTest/SkillSelect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnDClassesTest
+{
+    public partial class SkillSelect : Form
+    {
+        public SkillSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void SkillSelect_Load(object sender, EventArgs e)
+        {
+  //          bool[] classSkills = NewChar._class.ClassSkills();
+            for(int i = 0; i < 18; ++i)
+            {
+                //if (classSkills[i])checklistSkills.
+            }
+        }
+    }
+}
---
+++ DnDClassesTest/DnDClassesTest/StatsAndProfession.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DnDClassesTest
+{
+    public partial class StatsAndProfession : Form
+    {
+        public StatsAndProfession()
+        {
+            InitializeComponent();
+        }
+        public int p { get; set; }
+        public int proPath { get; set; }
+        public int level { get; set; }
+
+        private void comboClass_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            switch (comboClass.SelectedIndex)
+            {
+                case 0:
+                    lblSubClass.Text = "Choose a Path:";
+                    lblSubClass.Visible = true;
+                    comboSubClass.DataSource = new List<string> { "Berserker", "Totem Warrior" };
+                    break;
+            }
+
+        }
+
+        private void btnNext_Click(object sender, System.EventArgs e)
+        {
+            p = comboClass.SelectedIndex;
+            proPath = comboSubClass.SelectedIndex;
+            level = (int)numLevel.Value;
+            DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
