7e9e76b18898495ec2ef4b6570f2bc74a563d263
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-04-22 19:41:14 -0400

    sdjfgisehf

--- DnDMasterGenerator/DnDMasterGenerator/PDF Filler.cs
+++ DnDMasterGenerator/DnDMasterGenerator/PDF Filler.cs
@@ -20,16 +20,16 @@ namespace DnDClassesTest
 
         public void buildPDF()
         {
-            string nameOfFile = "";
+            string nameOfFile = "Test";
             string PDFFolder = Path.Combine(Environment.CurrentDirectory, @"..\..\PDFs");
-            string pdfTemplate =  PDFFolder  + @"\TWC-DnD-5E-Character-Sheet-v1.3";
+            string pdfTemplate =  PDFFolder  + @"\TWC-DnD-5E-Character-Sheet-v1.3.pdf";
             string newFile = PDFFolder + @"\" + nameOfFile + ".pdf";
 
             PdfReader reader = new PdfReader(pdfTemplate);
             PdfStamper pdfStamper = new PdfStamper(reader, new FileStream(newFile, FileMode.Create));
             AcroFields pdfFormFields = pdfStamper.AcroFields;
 
-            pdfFormFields.SetField("f1_91(0)", "blah");
+            pdfFormFields.SetField("Background", "blah");
         }
     }
 }
