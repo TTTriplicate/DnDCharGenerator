e84015eb1d0059fa23538a088caed38b0d6e0acb
WargG <warg.gallaher@gmail.com>
2019-04-22 22:20:11 -0400

    ItIsSoLate

--- DnDMasterGenerator/DnDMasterGenerator/AttackRoll.Designer.cs
+++ DnDMasterGenerator/DnDMasterGenerator/AttackRoll.Designer.cs
@@ -28,49 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Weapons = new System.Windows.Forms.ComboBox();
+            this.WeaponsLabel = new System.Windows.Forms.Label();
+            this.Roll = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.Hit = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.RollResult = new System.Windows.Forms.Label();
+            this.Miss = new System.Windows.Forms.RadioButton();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.damageFactor = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // Weapons
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 30);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 0;
+            this.Weapons.FormattingEnabled = true;
+            this.Weapons.Location = new System.Drawing.Point(12, 30);
+            this.Weapons.Name = "Weapons";
+            this.Weapons.Size = new System.Drawing.Size(121, 24);
+            this.Weapons.TabIndex = 0;
+            this.Weapons.SelectedIndexChanged += new System.EventHandler(this.Weapons_SelectedIndexChanged);
             // 
-            // label1
+            // WeaponsLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Weapons";
+            this.WeaponsLabel.AutoSize = true;
+            this.WeaponsLabel.Location = new System.Drawing.Point(12, 10);
+            this.WeaponsLabel.Name = "WeaponsLabel";
+            this.WeaponsLabel.Size = new System.Drawing.Size(68, 17);
+            this.WeaponsLabel.TabIndex = 1;
+            this.WeaponsLabel.Text = "Weapons";
             // 
-            // button1
+            // Roll
             // 
-            this.button1.Location = new System.Drawing.Point(91, 221);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 36);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Roll 1d20";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Roll.Location = new System.Drawing.Point(15, 78);
+            this.Roll.Name = "Roll";
+            this.Roll.Size = new System.Drawing.Size(111, 36);
+            this.Roll.TabIndex = 2;
+            this.Roll.Text = "Roll 1d20";
+            this.Roll.UseVisualStyleBackColor = true;
+            this.Roll.Click += new System.EventHandler(this.Roll_Click);
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.Miss);
             this.panel2.Controls.Add(this.Hit);
-            this.panel2.Location = new System.Drawing.Point(61, 263);
+            this.panel2.Location = new System.Drawing.Point(12, 156);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(169, 36);
+            this.panel2.Size = new System.Drawing.Size(152, 36);
             this.panel2.TabIndex = 4;
             // 
             // Hit
@@ -83,26 +87,48 @@
             this.Hit.TabStop = true;
             this.Hit.Text = "Hit";
             this.Hit.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(112, 4);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(57, 21);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Miss";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.Hit.CheckedChanged += new System.EventHandler(this.Hit_CheckedChanged);
             // 
             // RollResult
             // 
             this.RollResult.AutoSize = true;
-            this.RollResult.Location = new System.Drawing.Point(136, 191);
+            this.RollResult.Location = new System.Drawing.Point(85, 126);
             this.RollResult.Name = "RollResult";
             this.RollResult.Size = new System.Drawing.Size(16, 17);
             this.RollResult.TabIndex = 6;
             this.RollResult.Text = "0";
+            this.RollResult.Click += new System.EventHandler(this.RollResult_Click);
+            // 
+            // Miss
+            // 
+            this.Miss.AutoSize = true;
+            this.Miss.Location = new System.Drawing.Point(64, 4);
+            this.Miss.Name = "Miss";
+            this.Miss.Size = new System.Drawing.Size(57, 21);
+            this.Miss.TabIndex = 7;
+            this.Miss.TabStop = true;
+            this.Miss.Text = "Miss";
+            this.Miss.UseVisualStyleBackColor = true;
+            this.Miss.CheckedChanged += new System.EventHandler(this.Miss_CheckedChanged);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(15, 126);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(48, 17);
+            this.ResultLabel.TabIndex = 8;
+            this.ResultLabel.Text = "Result";
+            // 
+            // damageFactor
+            // 
+            this.damageFactor.AutoSize = true;
+            this.damageFactor.Location = new System.Drawing.Point(17, 211);
+            this.damageFactor.Name = "damageFactor";
+            this.damageFactor.Size = new System.Drawing.Size(109, 17);
+            this.damageFactor.TabIndex = 9;
+            this.damageFactor.Text = "Damage Factor:";
+            this.damageFactor.Click += new System.EventHandler(this.damageFactor_Click);
             // 
             // AttackRoll
             // 
@@ -110,11 +136,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(311, 375);
+            this.Controls.Add(this.damageFactor);
+            this.Controls.Add(this.ResultLabel);
             this.Controls.Add(this.RollResult);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Roll);
+            this.Controls.Add(this.WeaponsLabel);
+            this.Controls.Add(this.Weapons);
             this.Name = "AttackRoll";
             this.Text = "AttackRoll";
             this.Load += new System.EventHandler(this.AttackRoll_Load);
@@ -127,12 +155,14 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Weapons;
+        private System.Windows.Forms.Label WeaponsLabel;
+        private System.Windows.Forms.Button Roll;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton Hit;
         private System.Windows.Forms.Label RollResult;
+        private System.Windows.Forms.RadioButton Miss;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label damageFactor;
     }
 }
--- DnDMasterGenerator/DnDMasterGenerator/AttackRoll.cs
+++ DnDMasterGenerator/DnDMasterGenerator/AttackRoll.cs
@@ -12,12 +12,91 @@ namespace DnDClassesTest
 {
     public partial class AttackRoll : Form
     {
+        public int result = 0;
+        public List<Gear> weapons = new List<Gear>();
+        public List<string> weaponsString = new List<string>();
+        public List<Gear> inventory = new List<Gear>();
+        public CharGear gear = new CharGear();
         public AttackRoll()
         {
             InitializeComponent();
         }
 
         private void AttackRoll_Load(object sender, EventArgs e)
+        {
+            damageFactor.Hide();
+            inventory = gear.getInventory();
+
+            for(int i = 0; i < inventory.Count(); i++)
+            {
+                if (inventory[i].GetType().ToString() == "DnDClassesTest.Weapon")
+                {
+                    weapons.Add(inventory[i]);
+
+                }
+            }
+            for(int i = 0; i < weapons.Count(); i++)
+            {
+                weaponsString.Add(weapons[i].toString());
+            }
+            Weapons.DataSource = weaponsString;
+        }
+
+        private void Weapons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RollResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Roll_Click(object sender, EventArgs e)
+        {
+            Hit.Checked = false;
+            Miss.Checked = false;
+            damageFactor.Hide();
+            result = rollDice();
+            RollResult.Text = result.ToString();
+            if (result == 1)
+            {
+                Miss.Checked = true;
+            }
+            else if (result == 20)
+            {
+                Hit.Checked = true;
+            }
+        }
+
+        private void Hit_CheckedChanged(object sender, EventArgs e)
+        {
+            if (result == 1)
+            {
+                Miss.Checked = true;
+            }
+            else
+            {
+                //rollDice(1, weapons[Weapons.SelectedIndex].getDamage());
+            }
+
+        }
+
+        private void Miss_CheckedChanged(object sender, EventArgs e)
+        {
+            if (result == 20)
+            {
+                Hit.Checked = true;
+            }
+        }
+
+        public int rollDice(int low = 1, int high = 20)
+        {
+            Random random = new Random();
+            return random.Next(low, high);
+        }
+
+        private void damageFactor_Click(object sender, EventArgs e)
         {
 
         }
