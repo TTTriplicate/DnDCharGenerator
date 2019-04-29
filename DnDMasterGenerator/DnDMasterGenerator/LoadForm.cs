7a9877fde5ee4aa5f6de142712c094a9b70e5d55
Raidienaty <49071727+Raidienaty@users.noreply.github.com>
2019-04-29 10:27:03 -0400

    Worked on new load sheet

--- DnDMasterGenerator/DnDMasterGenerator/LoadForm.cs
+++ DnDMasterGenerator/DnDMasterGenerator/LoadForm.cs
@@ -73,6 +73,71 @@ namespace DnDClassesTest
             ArmorClass.Text = leeroy.getAC().ToString();
             Initiative.Text = leeroy.AbilityModifiers()[1].ToString();
             Speed.Text = leeroy.CharRace.getSpeed().ToString();
+
+            //SkillRadioButtons
+            count = 0;
+            bool[] skills = new bool[18];
+            System.Windows.Forms.RadioButton[] SkillButtons = { AcrobaticsButton, AHButton, ArcanaButton, AthleticsButton, DeceptionButton, HistoryButton, InsightButton, IntimidationButton, InvestigationButton, MedicineButton, NatureButton, PerceptionButton, PerformanceButton, PersuasionButton, ReligionButton, SleightOfHandButton, StealthButton, SurvivalButton };
+            foreach (bool i in leeroy._skills)
+            {
+                if (i == true)
+                {
+                    SkillButtons[count].Checked = true;
+                    skills[count] = true;
+                    MessageBox.Show(SkillButtons[count].Name.ToString() + " " +SkillButtons[count].ToString());
+                    //SkillButtons[count].L = false;
+                }
+                else
+                    SkillButtons[count].Checked = false;
+                count++;
+            }
+
+            //Skills
+            System.Windows.Forms.RichTextBox[] dex = { Acrobatics, SleightOfHand, Stealth, Athletics };
+            int[] dexIndexs = { 0, 15, 16, 3};
+            System.Windows.Forms.RichTextBox[] intel = { Arcana, History, Investigation, Nature, Religion };
+            int[] intelIndexs = { 2, 5, 8, 10, 14 };
+            System.Windows.Forms.RichTextBox[] wis = { AnimalHandling, Insight, Medicine, Perception, Survival };
+            int[] wisIndexs = { 1, 6, 9, 11, 17 };
+            System.Windows.Forms.RichTextBox[] cha = { Deception, Intimidation, Performance, Persuasion };
+            int[] chaIndexs = { 4, 7, 12, 13 };
+
+            string indexNum = "0";
+            foreach (var i in dex)
+            {
+                //Must do radio buttons first
+            }
+            //foreach (string i in dex)
+            //{
+            //    if (i.Length < 3)
+            //    {
+            //        indexNum = i;
+            //    }
+            //    else if (i.Length > 3)
+            //    {
+            //        if (i == "Athletics")
+            //        {
+            //            if (skillBoxes[int.Parse(indexNum)])
+            //            {
+            //                pdfFormFields.SetField(i, (newChar.AbilityModifiers()[0] + newChar.ProficiencyBonus()).ToString());
+            //                //MessageBox.Show("bingo");
+            //            }
+            //            else
+            //                pdfFormFields.SetField(i, newChar.AbilityModifiers()[0].ToString());
+            //        }
+            //        //MessageBox.Show((int.Parse(indexNum).ToString() + skillBoxes[int.Parse(indexNum)].ToString()));
+            //        else if (skillBoxes[int.Parse(indexNum)])
+            //        {
+            //            pdfFormFields.SetField(i, (newChar.AbilityModifiers()[1] + newChar.ProficiencyBonus()).ToString());
+            //            //MessageBox.Show("bingo");
+            //        }
+            //        else
+            //            pdfFormFields.SetField(i, newChar.AbilityModifiers()[1].ToString());
+            //    }
+            //    //indexNum = "0";
+            //}
+
+            
         }
         private void CharName_TextChanged(object sender, EventArgs e)
         {
@@ -81,9 +146,7 @@ namespace DnDClassesTest
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            languages.SelectAll();
-            languages.SelectionAlignment = HorizontalAlignment.Center;
-            languages.DeselectAll();
+            
         }
 
         private void label17_Click(object sender, EventArgs e)
@@ -144,6 +207,13 @@ namespace DnDClassesTest
         private void Equipment_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void STR_TextChanged(object sender, EventArgs e)
+        {
+            STR.SelectAll();
+            STR.SelectionAlignment = HorizontalAlignment.Center;
+            STR.DeselectAll();
         }
     }
 }
