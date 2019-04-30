6df1660a38e47a60d64079e271035d8e12acf919
TTTriplicate <csousa68w@gmail.com>
2019-04-30 11:20:03 -0400

    Fighter Level
    And removed redundant fighting style form

--- DnDMasterGenerator/DnDMasterGenerator/Professions/PickOne.cs
+++ DnDMasterGenerator/DnDMasterGenerator/Professions/PickOne.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,19 @@ namespace DnDMasterGenerator.Professions
             comboSelector.DataSource = Options;
         }
 
+        public PickOne(string pro)
+        {
+            InitializeComponent();
+            Choices = this.FightingStyles(pro);
+            Options = new List<string>();
+            foreach (string i in Choices)
+            {
+                Options.Add(i.Split(':')[0]);
+            }
+            comboSelector.DataSource = Options;
+
+        }
+
         private void comboSelector_SelectedIndexChanged(object sender, EventArgs e)
         {
             txtSelectionDisplay.Text = Choices[comboSelector.SelectedIndex];
@@ -44,5 +58,29 @@ namespace DnDMasterGenerator.Professions
             DialogResult = DialogResult.OK;
             this.Close();
         }
+
+        private List<string> FightingStyles(string pro)
+        {
+            List<string> styles = new List<string>();
+            string path = Path.Combine(Environment.CurrentDirectory, @"..\..\Professions\FightingStyles\FightingStyles.txt");
+            //string path = @"C:\Users\csous\source\repos\DnDClassesTest\DnDClassesTest\Professions\ClassFeatures\BarbarianClassFeatures.txt";
+            string[] temp = new string[6];
+            temp = File.ReadAllLines(path);
+            foreach (string i in temp)
+            {
+                styles.Add(i);
+            }
+            if (pro == "Paladin")
+            {
+                styles.RemoveAt(0);
+                styles.RemoveAt(4);
+            }
+            if (pro == "Ranger")
+            {
+                styles.RemoveRange(3, 2);
+            }
+            return styles;
+        }
     }
+}
 }
